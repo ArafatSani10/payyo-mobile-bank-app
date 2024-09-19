bd841f75a906fe60bc11f4254305995700cfba74
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2024-09-19 18:48:12 +0600

    partially add money event inprogress.

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
-console.log('home added')
+// add money to the account 
+
+// step-01-> add an event handlar to the add money button inside the form 
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+    // step-02
+    let addMoneyInput = document.getElementById('input-add-money').value;
+    let addPinInput = document.getElementById('input-pin-money').value;
+    console.log(addMoneyInput,addPinInput);
+ 
+
+
+})
