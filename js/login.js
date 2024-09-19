30692b74251e45d4bb1e045b0814d6edff0b2774
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2024-09-19 16:22:39 +0600

    added a phone number and pin number and check number and pin and login option..and create a onclick page upload.

--- js/login.js
+++ js/login.js
@@ -1,11 +1,40 @@
 console.log('button cliking file added ')
-// step 01 -> set event handlar
+// // step 01 -> set event handlar
+// document.getElementById('button-login').addEventListener('click', function(event){
+//     // step 02-> set pereventDefaut behaviour
+//     event.preventDefault();
+//     // console.log('login button clicked')
+//     // step 03 -> get element by id get phone number
+//     let phoneNumber = document.getElementById('phone-number').value;
+//     let pinNumber = document.getElementById('pin-number').value;
+
+//     console.log(phoneNumber, pinNumber);
+
+//     // this is temporary..you should do like this
+//     // step -4
+//     if(phoneNumber === '01747365915' && pinNumber === '12345'){
+//         console.log('you are logged in')
+//     }
+//     else{
+//         alert('wrong phone number or pin')
+//     }
+  
+
+// })
+
+
 document.getElementById('button-login').addEventListener('click', function(event){
-    // step 02-> set pereventDefaut behaviour
     event.preventDefault();
-    console.log('login button clicked')
-    // step 03 -> get element by id get phone number
     let phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber )
+    let pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber);
+    if(phoneNumber === '01747365915' && pinNumber === '12345'){
+        console.log('You are logged in')
+        window.location.href='/home.html'
+    }
+    else{
+        alert('wrong phone number or pin number')
+    }
+   
 
 })
